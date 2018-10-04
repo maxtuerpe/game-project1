261540eda691090c80b664255f29af181287e08a
Max Tuerpe <maxtuerpe4@gmail.com>
2018-10-04 14:56:59 -0600

    functions to meathods

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -12,45 +12,145 @@ const backgrounds = [
 ]
 $('body').css({'background-color': backgrounds[Math.floor(Math.random() * 5)]});
 let gameSpeed = 4;
-
 const bird = {
     x: 5,
     y: 5,
     missles: 20,
     score: 0,
+    fireMissle(){
+        if (this.missles > 0){
+            const missle = new Missle(this.x, this.y);
+            $(`.square-${missle.x}-${missle.y}`).addClass('missle');
+            missle.move();
+            missle.destroy();
+            this.missles--;
+        }
+    },
+    moveUp(){
+        if(this.y < 10){
+            const currentSquare = $('#bird');
+            currentSquare.removeAttr('id');
+            this.y++;
+            $(`.square-${this.x}-${this.y}`).removeClass('hole');
+            $(`.square-${this.x}-${this.y}`).attr('id', 'bird');
+        }
+    },
+    moveDown(){
+        if(this.y > 1){
+            const currentSquare = $('#bird');
+            currentSquare.removeAttr('id');
+            this.y--;
+            $(`.square-${this.x}-${this.y}`).removeClass('hole');
+            $(`.square-${this.x}-${this.y}`).attr('id', 'bird');
+        }
+    }
 }
-$('#start-game').on('click', (e)=>{
-    (e.currentTarget).remove();
-    $('p').remove();
-    $('body').append("<div class='bottom'></div>")
-    makeBoard();
-    makeBird();
-    $('body').keydown((e)=>{
-        if(e.keyCode === 38){
-            $('.woosh')[0].play();
-            moveUp();
-    }});
-    $('body').keydown((e)=>{
-        if(e.keyCode === 40){
-            $('.woosh')[0].play();
-            moveDown();
-    }});
-    $('body').keydown((e)=>{
-        if(e.keyCode === 32){
-            $('.pew')[0].play();
-            fireMissle();
-    }});
-    setInterval(()=>{
-        createBar();
-    },5000/gameSpeed)
-    setTimeout(()=>{setInterval(()=>{
-        createCoin();
-    },5000/gameSpeed)},2000/gameSpeed) 
-    setInterval(()=>{
-        $('.score').text(`score: ${bird.score}`);
-        $('.missles').text(`missles: ${bird.missles}`);
-    }, 10/gameSpeed)
-}) 
+class Bar  { 
+    constructor( x, hole){
+    this.x = x;
+    this.hole = hole;
+    }
+    newBar(){
+        for (let i = 0; i < 10; i++){
+            if((i+1)!== this.hole){ 
+                $(`.square-${this.x}-${i+1}`).removeClass('blank');
+                $(`.square-${this.x}-${i+1}`).addClass('box');
+            } else {
+                $(`.square-${this.x}-${i+1}`).addClass('hole');
+            }
+        }
+    }
+    move(){  
+        setInterval(()=>{
+            if(this.x > 0){
+                this.x--;
+                for (let i = 0; i < 10; i++){
+                    if($(`.square-${this.x + 1}-${i+1}`).hasClass('box')){
+                        $(`.square-${this.x + 1}-${i+1}`).removeClass('box');
+                        $(`.square-${this.x + 1}-${i+1}`).addClass('blank');
+                        $(`.square-${this.x}-${i+1}`).addClass('box');
+                    } if($(`.square-${this.x + 1}-${i+1}`).hasClass('hole')){
+                        $(`.square-${this.x + 1}-${i+1}`).removeClass('hole');
+                        $(`.square-${this.x + 1}-${i+1}`).addClass('blank');
+                        $(`.square-${this.x}-${i+1}`).addClass('hole');
+                    }
+                }   
+            gameOver();
+            }
+        }, 500/gameSpeed); 
+    }
+    explode(){
+        setInterval(()=>{
+            for (let i = 0; i < 10; i++){
+                if($(`.square-${this.x}-${i+1}`).hasClass('missle')){
+                    $('.punch')[0].play();
+                    $(`.square-${this.x}-${i+1}`).removeClass('box');
+                    $(`.square-${this.x}-${i+1}`).addClass('hole');
+                }
+            }
+        }, 1);
+    }
+}
+class Coin {
+    constructor(y){
+        this.y = y;
+        this.x = 26;
+        this.active = true;
+    }
+    move(){
+        setInterval(()=>{
+            if(this.x > 0){
+                $(`.square-${this.x}-${this.y}`).removeClass('coin');
+                if(this.active){
+                this.x--;
+                $(`.square-${this.x + 1}-${this.y}`).addClass('blank');
+                $(`.square-${this.x}-${this.y}`).addClass('coin');
+                }  
+            }
+        }, 500/gameSpeed)     
+    }
+    score(){
+        setInterval(()=>{
+            if(this.active){
+                if($(`.square-${this.x}-${this.y}`)[0].hasAttribute('id', 'bird')){ 
+                    bird.score++;
+                    $(`.square-${this.x}-${this.y}`).removeClass('coin');
+                    this.active = false;  
+                }
+            } 
+        }, 5)    
+    } 
+    newCoin(){
+        $(`.square-${this.x}-${this.y}`).removeClass('blank');
+        $(`.square-${this.x}-${this.y}`).addClass('coin');
+    }     
+} 
+class Missle {
+    constructor(x, y){
+        this.x = x;
+        this.y = y; 
+        this.active = true;
+    }
+    move(){
+        setInterval(()=>{
+            this.x++;
+            if(this.active){
+                $(`.square-${this.x - 1}-${this.y}`).removeClass('missle');
+                $(`.square-${this.x - 1}-${this.y}`).addClass('blank');
+                $(`.square-${this.x}-${this.y}`).addClass('missle');
+            } else {
+                $(`.square-${this.x - 1}-${this.y}`).removeClass('missle');
+            } 
+        },50)  
+    }
+    destroy(){
+        setInterval(()=>{
+            if($(`.square-${this.x}-${this.y}`).hasClass('box')){
+            this.active = false;
+            }
+        },1)   
+    } 
+}
 const hardMode = () => {
     gameSpeed+= .5;
 }
@@ -78,139 +178,46 @@ const makeBoard = () => {
         }
     }
 }
-const createBar = () => {
-    const bar = new Bar(25, Math.ceil(Math.random()*10))
-    for (let i = 0; i < 10; i++){
-        if((i+1)!== bar.hole){ 
-            $(`.square-${bar.x}-${i+1}`).removeClass('blank');
-            $(`.square-${bar.x}-${i+1}`).addClass('box');
-        } else {
-            $(`.square-${bar.x}-${i+1}`).addClass('hole');
-        }
-    }
+$('#start-game').on('click', (e)=>{
+    (e.currentTarget).remove();
+    $('p').remove();
+    $('body').append("<div class='bottom'></div>")
+    makeBoard();
+    makeBird();
+    $('body').keydown((e)=>{
+        if(e.keyCode === 38){
+            $('.woosh')[0].play();
+            bird.moveUp();
+    }});
+    $('body').keydown((e)=>{
+        if(e.keyCode === 40){
+            $('.woosh')[0].play();
+            bird.moveDown();
+    }});
+    $('body').keydown((e)=>{
+        if(e.keyCode === 32){
+            $('.pew')[0].play();
+            bird.fireMissle();
+    }});
     setInterval(()=>{
-        for (let i = 0; i < 10; i++){
-            if($(`.square-${bar.x}-${i+1}`).hasClass('missle')){
-                $('.punch')[0].play();
-                $(`.square-${bar.x}-${i+1}`).removeClass('box');
-                $(`.square-${bar.x}-${i+1}`).addClass('hole');
-            }
-        }
-    }, 1);
-    setInterval(()=>{
-        if(bar.x > 0){
-            bar.move();
-            gameOver();
-    }}, 500/gameSpeed);
-}
-const createCoin = () => {
-    const coin = new Coin(Math.ceil(Math.random()*10))
-    $(`.square-${coin.x}-${coin.y}`).removeClass('blank');
-    $(`.square-${coin.x}-${coin.y}`).addClass('coin');
-    setInterval(()=>{
-        if(coin.x > 0){
-            coin.move();
-        }
-    }, 500/gameSpeed)
-    setInterval(()=>{
+        const bar = new Bar(25, Math.ceil(Math.random()*10))
+        bar.newBar();
+        bar.explode();
+        bar.move();
+    },5000/gameSpeed)
+    setTimeout(()=>{setInterval(()=>{
+        const coin = new Coin(Math.ceil(Math.random()*10))
+        coin.newCoin();
+        coin.move();
         coin.score();
-    }, 5)
-}
-const fireMissle = () => {
-    if (bird.missles > 0){
-        const missle = new Missle(bird.x, bird.y);
-        $(`.square-${missle.x}-${missle.y}`).addClass('missle');
-        setInterval(()=>{
-            missle.move();
-            missle.destroy();
-        }, 50);
-        bird.missles--;
-    }
-}
-const moveUp = () => {
-    if(bird.y < 10){
-        const currentSquare = $('#bird');
-        currentSquare.removeAttr('id');
-        bird.y++;
-        $(`.square-${bird.x}-${bird.y}`).removeClass('hole');
-        $(`.square-${bird.x}-${bird.y}`).attr('id', 'bird');
-    }
-}
-const moveDown = () => {
-    if(bird.y > 1){
-        const currentSquare = $('#bird');
-        currentSquare.removeAttr('id');
-        bird.y--;
-        $(`.square-${bird.x}-${bird.y}`).removeClass('hole');
-        $(`.square-${bird.x}-${bird.y}`).attr('id', 'bird');
-    }
-}
-class Bar  { 
-    constructor( x, hole){
-    this.x = x;
-    this.hole = hole;
-    } 
-    move(){  
-        this.x--;
-        for (let i = 0; i < 10; i++){
-            if($(`.square-${this.x + 1}-${i+1}`).hasClass('box')){
-                $(`.square-${this.x + 1}-${i+1}`).removeClass('box');
-                $(`.square-${this.x + 1}-${i+1}`).addClass('blank');
-                $(`.square-${this.x}-${i+1}`).addClass('box');
-            } if($(`.square-${this.x + 1}-${i+1}`).hasClass('hole')){
-                $(`.square-${this.x + 1}-${i+1}`).removeClass('hole');
-                $(`.square-${this.x + 1}-${i+1}`).addClass('blank');
-                $(`.square-${this.x}-${i+1}`).addClass('hole');
-            }
-        }   
-    }
-}
-class Coin {
-    constructor(y){
-        this.y = y;
-        this.x = 26;
-        this.active = true;
-    }
-    move(){
-        $(`.square-${this.x}-${this.y}`).removeClass('coin');
-        if(this.active){
-            this.x--;
-            $(`.square-${this.x + 1}-${this.y}`).addClass('blank');
-            $(`.square-${this.x}-${this.y}`).addClass('coin');
-        }       
-    }
-    score(){
-        if(this.active){
-            if($(`.square-${this.x}-${this.y}`)[0].hasAttribute('id', 'bird')){ 
-                bird.score++;
-                $(`.square-${this.x}-${this.y}`).removeClass('coin');
-                this.active = false;  
-            }
-        }   
-    }      
-} 
-class Missle {
-    constructor(x, y){
-        this.x = x;
-        this.y = y; 
-        this.active = true;
-    }
-    move(){
-        this.x++;
-        if(this.active){
-            $(`.square-${this.x - 1}-${this.y}`).removeClass('missle');
-            $(`.square-${this.x - 1}-${this.y}`).addClass('blank');
-            $(`.square-${this.x}-${this.y}`).addClass('missle');
-        } else {
-            $(`.square-${this.x - 1}-${this.y}`).removeClass('missle');
-        } 
-    }
-    destroy(){
-        if($(`.square-${this.x}-${this.y}`).hasClass('box')){
-            this.active = false;
-        }
-    } 
-}
+    },5000/gameSpeed)},2000/gameSpeed) 
+    setInterval(()=>{
+        $('.score').text(`score: ${bird.score}`);
+        $('.missles').text(`missles: ${bird.missles}`);
+    }, 10/gameSpeed)
+}) 
+
+
 
 
 
@@ -221,7 +228,7 @@ class Missle {
 
 $('body').keydown((e)=>{
     if(e.keyCode === 83){
-        score+= 234;
+        bird.score+= 234;
     }
 })
 
